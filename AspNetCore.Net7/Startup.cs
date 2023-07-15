64b6b3c2b513637ab6fd46add66139694fcab382
kirypto <kirypto@gmail.com>
2023-07-15 17:06:43 -0600

    Add an AspNetCore (dot net version 7) project template

---
+++ AspNetCore.Net7/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace kirypto.Template.AspNetCore.Net7;
+
+public class Startup {
+    public void ConfigureServices(IServiceCollection services) {
+        // Add any services your application requires
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        if (env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints => {
+            endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello World!"));
+        });
+    }
+}
